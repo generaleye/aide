9775a065b881f889c7a61ae00a1aecedf5fe486b
generaleye <odumuyiwaleye@gmail.com>
2015-02-28 03:08:42 +0100

    fix config file

--- include/Config.php
+++ include/Config.php
@@ -3,15 +3,18 @@
  * Database configuration
  * Author: Generaleye
  */
-//define('DB_HOST', 'localhost');
-//define('DB_USERNAME', 'root');
-//define('DB_PASSWORD', 'root');
-//define('DB_NAME', 'aide');
 
-define('DB_HOST', '127.10.196.130');
-define('DB_USERNAME', 'adminxAsHCnN');
-define('DB_PASSWORD', 'Z4ZNpUd9Vfwv');
-define('DB_NAME', 'aide');
+if ($_SERVER["SERVER_NAME"]=="localhost") {
+    define('DB_HOST', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', 'root');
+    define('DB_NAME', 'aide');
+} elseif ($_SERVER["SERVER_NAME"]=="aide-generaleye.rhcloud.com") {
+    define('DB_HOST', '127.10.196.130');
+    define('DB_USERNAME', 'adminxAsHCnN');
+    define('DB_PASSWORD', 'Z4ZNpUd9Vfwv');
+    define('DB_NAME', 'aide');
+}
 
 define('REGISTRATION_SUCCESSFUL', 0);
 define('REGISTRATION_FAILED', 1);
